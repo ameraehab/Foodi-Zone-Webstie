ce3979e9934692c2b8f118edf411fe68f68f39b5
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-06-11 21:05:45 +0300

    edite responsive feature

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -45,15 +45,17 @@ const Hero = () => {
 text-center gap-4 pt-12 sm:pt-0 order-2 sm:order-1"
             >
               <h1
-                className="text-5xl sm:text-6xl lg:text-7xl
+                className="text-5xl  lg:text-7xl sm:ml-5 sm:mt-8 
    font-bold"
               >
                 Welcom to the Foodie Zone
               </h1>
-              <p className="text-sm">Discover the best food in town</p>
+              <p className="text-md sm:ml-5 sm:mt-5">
+                Discover the best food in town
+              </p>
               <button
                 className="bg-gradient-to-r from-primary
-    to-secondary text-white px-4 py-2 
+    to-secondary text-white px-4 py-2 w-70  sm:ml-5 ml-[23%]
     rounded-full duration-200 cursor-pointer hover:scale-105  "
               >
                 Order Now
@@ -68,20 +70,26 @@ text-center gap-4 pt-12 sm:pt-0 order-2 sm:order-1"
               {/* main image section */}
               <div
                 className="flex justify-center items-center
-               h-[300px] sm:h-[450px] overflow-hidden"
+               h-[300px] lg:w-[655px] sm:h-[525px] overflow-hidden"
               >
                 <img
                   src={imageId}
                   alt=""
-                  className="w-[300px] sm:w-[450px] 
-                  mx-auto animate-spin [animation-duration:40s]"
+                  className="w-[300px] md:mr-[-12px] lg:w-[450px] 
+                  lg:mt-18 
+                  lg:ml-[-104px] sm:w-[380px] 
+                  mx-auto animate-spin 
+                  [animation-duration:40s]"
                 />
               </div>
               {/*  image List section */}
               <div
-                className="flex lg:flex-col lg:top-1/2 
-              lg:-translate-y-1/2 lg:py-2 justify-center gap-4 absolute bottom-[0px]
-               lg:-right-0 bg-white/30 rounded-full p-2"
+                className="flex  lg:flex-col lg:right-10
+                 md:mr-[-40px] 
+                sm:right-15
+              sm:-translate-y-1/2  justify-center 
+              gap-4 absolute bottom-[-25px] md:bottom-[-50px]
+                bg-white/70 rounded-full p-0.5"
               >
                 {ImageList.map((image) => (
                   <img
