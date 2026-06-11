bc7f9e45c12799030e07032dd0c933454b4a8305
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-06-11 21:12:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Hero from "./components/Hero/Hero"
-import Navbar from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Hero/>
+      <Navbar />
+      <Hero />
+      <Services />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
