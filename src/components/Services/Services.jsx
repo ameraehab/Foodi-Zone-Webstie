606a0617b4953e9906ae9c7d85e7daa400d0a17f
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-06-14 21:30:56 +0300

    add new services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -32,10 +32,7 @@ const Services = () => {
       <div className="py-10">
         <div className="container">
           {/*Header section*/}
-          <div
-            className="text-center mb-20 
-          max-w-100  mx-auto "
-          >
+          <div className="text-center mb-20 mx-auto  ">
             <p
               className="text-xl bg-clip-text text-transparent
              bg-gradient-to-r from-primary to-secondary"
@@ -43,13 +40,46 @@ const Services = () => {
               Our Services
             </p>
             <h1 className="text-4xl font-bold">Services</h1>
-            <p className="text-gray-500 text-xs">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis a facilis similique saepe minima perspiciatis iure illo
-              iusto nam mollitia?
+            <p className="text-gray-600 text-sm mt-2">
+              Experience the perfect blend of taste, quality, and exceptional
+              service.
             </p>
             {/*Card section*/}
-            <div></div>
+            <div>
+              <div
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
+                  mt-16 place-items-center  "
+              >
+                {servicesImages.map(({ id, img, name, description }) => {
+                  return (
+                    <div
+                      key={id}
+                      className=" mt-5 w-72 bg-orange-50 rounded-2xl p-4 pt-12
+    text-center shadow-xl dark:bg-gray-800 group  hover:bg-primary
+     hover:text-white duration-300 mb-15 hover:scale-105 text-black cursor-pointer dark:text-white"
+                    >
+                      <img
+                        src={img}
+                        alt={name}
+                        className="w-32 mx-auto -mt-[110px] 
+                        group-hover:scale-105
+                       group-hover:rotate-12 duration-300"
+                      />
+
+                      <div className="px-3 pb-4">
+                        <h2 className="text-xl font-bold ">{name}</h2>
+                        <p
+                          className="text-gray-500 text-sm 
+                        line-clamp-5 mt-2"
+                        >
+                          {description}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </div>
