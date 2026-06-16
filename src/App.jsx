62d6586a209e2de334947c3fe8ad6573f6a622be
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-06-17 02:23:45 +0300

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AppStore from "./components/AppStore/AppStore";
 import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
@@ -12,6 +13,7 @@ const App = () => {
       <Services />
       <Banner />
       <AppStore />
+      <Footer />
     </>
   );
 };
