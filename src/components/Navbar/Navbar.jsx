876d1cef893bf23f6c7601e5aab77c0613faf990
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2026-06-17 01:13:17 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,65 +1,76 @@
-import { FaShoppingCart } from "react-icons/fa"
-import Logo from "../../assets/food-logo.png"
-import Darkmode from "./Darkmode"
+import { FaShoppingCart } from "react-icons/fa";
+import Logo from "../../assets/food-logo.png";
+import Darkmode from "./Darkmode";
 const Navbar = () => {
   return (
-    <div className="shadow-md bg-white
-     dark:bg-gray-900 dark:text-white duration-200">
+    <div
+      className="shadow-md bg-white fixed top-0 left-0 w-full z-50 bg-white shadow-md
+     dark:bg-gray-900 dark:text-white duration-200"
+    >
       <div className="container-custom py-2  sm-py-0">
         <div className="flex justify-between items-center ">
-{/* Logo Section */}
-           <div> 
-              <a href="#" className="flex items-center gap-2 font-bold text-2xl sm:text-3xl">
-                <img src={Logo} alt="Foodie Zone"
-                className="w-10 h-10"/>
-                    Foodie
-              </a>    
+          {/* Logo Section */}
+          <div>
+            <a
+              href="#"
+              className="flex items-center gap-2 font-bold text-2xl sm:text-3xl"
+            >
+              <img src={Logo} alt="Foodie Zone" className="w-10 h-10" />
+              Foodie
+            </a>
+          </div>
+
+          {/* List Section */}
+          <div className="flex items-center gap-4 ">
+            {/*Dark Mode Toggle Button */}
+            <div className="">
+              <Darkmode />
             </div>
-
-
-
-        { /* List Section */} 
-            <div className="flex items-center gap-4 ">  
-
-                   {/*Dark Mode Toggle Button */}
-              <div className="">
-                <Darkmode/>
-              </div>
-              <ul className="hidden sm:flex gap-3.5 ">
-                <li>
-                  <a href="#" className="inline-block px-4 py-4
+            <ul className="hidden sm:flex gap-3.5 ">
+              <li>
+                <a
+                  href="#"
+                  className="inline-block px-4 py-4
                    hover:text-primary
-                    ">Home</a>
-                </li>
-                <li>      
-                  <a href="#" className="inline-block px-4 py-4
+                    "
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="inline-block px-4 py-4
                                      hover:text-primary
-">Menu</a>
-                </li>
-                <li>
-                  <a href="#" className="inline-block px-4 py-4
-                   hover:text-primary">Contact</a>
-                </li>
-              </ul>
-         
-              {/* Order Button  */}
-              <button className="flex items-center gap-1 bg-gradient-to-r from-primary to-secondary
-              text-white px-6 py-2 rounded-full  hover:scale-105 duration-300 ">
-                Order
-<FaShoppingCart className="text-lg text-white drop-shadow-sm cursor-pointer"/>
-              </button>
-              
-             </div>
+"
+                >
+                  Menu
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="inline-block px-4 py-4
+                   hover:text-primary"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
 
-
-
-
-
-
+            {/* Order Button  */}
+            <button
+              className="flex items-center gap-1 bg-gradient-to-r from-primary to-secondary
+              text-white px-6 py-2 rounded-full  hover:scale-105 duration-300 "
+            >
+              Order
+              <FaShoppingCart className="text-lg text-white drop-shadow-sm cursor-pointer" />
+            </button>
+          </div>
         </div>
-         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
